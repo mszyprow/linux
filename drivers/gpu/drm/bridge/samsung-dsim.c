9915215c7e383f681a56886a434ad96e8a25468d
Jagan Teki <jagan@amarulasolutions.com>
2022-12-09 20:53:37 +0530

    drm: bridge: samsung-dsim: Add platform PLL_P (PMS_P) offset
    Look like PLL PMS_P offset value varies between platforms that have
    Samsung DSIM IP.
    
    However, there is no clear evidence for it as both Exynos and i.MX
    8M Mini Application Processor Reference Manual is still referring
    the PMS_P offset as 13.
    
    The offset 13 is not working for i.MX8M Mini SoCs but the downstream
    NXP sec-dsim.c driver is using offset 14 for i.MX8M Mini SoC platforms
    [1] [2].
    
    PMS_P value set in sec_mipi_dsim_check_pll_out using PLLCTRL_SET_P()
    with offset 13 and then an additional offset of one bit added in
    sec_mipi_dsim_config_pll via PLLCTRL_SET_PMS().
    
    Not sure whether it is reference manual documentation or something else
    but this patch trusts the downstream code and handle PLL_P offset via
    platform driver data so-that imx8mm driver data shall use pll_p_offset
    to 14.
    
    Similar to Mini the i.MX8M Nano/Plus also has P=14, unlike Exynos.
    
    [1] https://protect2.fireeye.com/v1/url?k=ebf0d876-8b12452b-ebf15339-000babd9f1ba-318a9422dff1af0a&q=1&e=42c48367-0834-4680-ba37-5a8337550639&u=https%3A%2F%2Fsource.codeaurora.org%2Fexternal%2Fimx%2Flinux-imx%2Ftree%2Fdrivers%2Fgpu%2Fdrm%2Fbridge%2Fsec-dsim.c%3Fh%3Dimx_5.4.47_2.2.0%23n210
    [2] https://protect2.fireeye.com/v1/url?k=f8085bdd-98eac680-f809d092-000babd9f1ba-357fc022f318a078&q=1&e=42c48367-0834-4680-ba37-5a8337550639&u=https%3A%2F%2Fsource.codeaurora.org%2Fexternal%2Fimx%2Flinux-imx%2Ftree%2Fdrivers%2Fgpu%2Fdrm%2Fbridge%2Fsec-dsim.c%3Fh%3Dimx_5.4.47_2.2.0%23n211
    
    v9:
    * none
    
    v8:
    * updated commit message for 8M Nano/Plus
    
    v7, v6:
    * none
    
    v5:
    * updated clear commit message
    
    v4, v3, v2:
    * none
    
    v1:
    * updated commit message
    * add downstream driver link
    
    Reviewed-by: Marek Vasut <marex@denx.de>
    Signed-off-by: Frieder Schrempf <frieder.schrempf@kontron.de>
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>

--- drivers/gpu/drm/bridge/samsung-dsim.c
+++ drivers/gpu/drm/bridge/samsung-dsim.c
@@ -184,7 +184,7 @@
 /* DSIM_PLLCTRL */
 #define DSIM_FREQ_BAND(x)		((x) << 24)
 #define DSIM_PLL_EN			(1 << 23)
-#define DSIM_PLL_P(x)			((x) << 13)
+#define DSIM_PLL_P(x, offset)		((x) << (offset))
 #define DSIM_PLL_M(x)			((x) << 4)
 #define DSIM_PLL_S(x)			((x) << 1)
 
@@ -384,6 +384,7 @@ static const struct samsung_dsim_driver_data exynos3_dsi_driver_data = {
 	.max_freq = 1000,
 	.wait_for_reset = 1,
 	.num_bits_resol = 11,
+	.pll_p_offset = 13,
 	.reg_values = reg_values,
 };
 
@@ -396,6 +397,7 @@ static const struct samsung_dsim_driver_data exynos4_dsi_driver_data = {
 	.max_freq = 1000,
 	.wait_for_reset = 1,
 	.num_bits_resol = 11,
+	.pll_p_offset = 13,
 	.reg_values = reg_values,
 };
 
@@ -406,6 +408,7 @@ static const struct samsung_dsim_driver_data exynos5_dsi_driver_data = {
 	.max_freq = 1000,
 	.wait_for_reset = 1,
 	.num_bits_resol = 11,
+	.pll_p_offset = 13,
 	.reg_values = reg_values,
 };
 
@@ -417,6 +420,7 @@ static const struct samsung_dsim_driver_data exynos5433_dsi_driver_data = {
 	.max_freq = 1500,
 	.wait_for_reset = 0,
 	.num_bits_resol = 12,
+	.pll_p_offset = 13,
 	.reg_values = exynos5433_reg_values,
 };
 
@@ -428,6 +432,7 @@ static const struct samsung_dsim_driver_data exynos5422_dsi_driver_data = {
 	.max_freq = 1500,
 	.wait_for_reset = 1,
 	.num_bits_resol = 12,
+	.pll_p_offset = 13,
 	.reg_values = exynos5422_reg_values,
 };
 
@@ -559,7 +564,8 @@ static unsigned long samsung_dsim_set_pll(struct samsung_dsim *dsi,
 	writel(driver_data->reg_values[PLL_TIMER],
 			dsi->reg_base + driver_data->plltmr_reg);
 
-	reg = DSIM_PLL_EN | DSIM_PLL_P(p) | DSIM_PLL_M(m) | DSIM_PLL_S(s);
+	reg = DSIM_PLL_EN | DSIM_PLL_P(p, driver_data->pll_p_offset) |
+	      DSIM_PLL_M(m) | DSIM_PLL_S(s);
 
 	if (driver_data->has_freqband) {
 		static const unsigned long freq_bands[] = {
